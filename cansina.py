43ddaf9ddba461381803b37b65f2928bc19ded20
David Garcia <daganu@gmail.com>
2013-02-26 00:54:17 +0100

    added arguments parsing

--- cansina.py
+++ cansina.py
@@ -14,6 +14,9 @@ from visitor import Visitor
 from payload import Payload
 from task import Task
 
+#
+# Parsing program options
+#
 parser = argparse.ArgumentParser()
 parser.add_argument('-u', dest = 'target', \
                         help = "target url", required = True)
@@ -30,40 +33,17 @@ payload_file = args.payload
 extension = args.extension
 threads = args.threads
 
-print target, payload_file, extension, threads
-sys.exit()
-
-
-site = sys.argv[1]
-if site[-1] == '/':
-    site = site[:-1]
-
-payload = sys.argv[2]
-
-extension = ""
-try:
-    extension = sys.argv[3]
-    print "Using %s extension" % extension
-except:
-    pass
-
-results = []
-banned = []
-
-payload_file = open(payload, 'r')
+#
+# Creating middle objects
+#
+payload_object = Payload(payload_file, extension)
 log_results = open("%s.log" % time.time(), 'w')
+
+
 
 
 def format_result(cad):
     return "{0[2]:^6} {0[1]:^10} {0[0]}{1}".format(cad, os.linesep)
-
-queue = Queue.Queue()
-for i in payload_file:
-    queue.put(i)
-payload_file.close()
-total_req = queue.qsize()
-current_req = 0
-lock = threading.Lock()
 
 if __name__ == '__main__':
     for n in range(0, THREADS):
